ecbdd070c6441fb79d34b4329d4c40d823a7efbc
ZackaryW <36378555+ZackaryW@users.noreply.github.com>
2024-02-09 10:27:17 -0800

    env: bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name="sioDict",
     description="Super Charged Io Dicts",
-    version="0.3.1",
+    version="0.3.2",
     packages=["sioDict", "sioDict.variants"],
     license="MIT",
     long_description=open("README.md").read(),
